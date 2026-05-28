482d52bc7ea4452639a8b30d0af2143513800eb5
Hyeonjin.K <gcd1324@gmail.com>
2026-05-28 12:19:53 +0900

    필요한 연습문제만 남김

--- src/practice2/SystemConstants.java
+++
@@ -1,9 +0,0 @@
-package practice2;
-
-public class SystemConstants {
-    public static final String HEADER_SYSTEM = "====== 통합 관리 시스템 ======";
-    public static final String PROMPT_SERVICE_CHOICE = "실행할 서비스를 선택하세요 : ";
-    public static final String MSG_EXIT = "통합 관리 시스템을 종료합니다.";
-    public static final String MSG_INVALID_CHOICE = "잘못 선택하셨습니다. 다시 입력해주세요.\n";
-    public static final String MSG_PARSE_ERROR = "잘못 입력하셨습니다. 숫자만 입력해 주세요.\n";
-}
--- src/practice2/product/dto/ProductInputDto.java
+++
@@ -1,7 +0,0 @@
-package practice2.product.dto;
-
-import practice2.product.type.ProductCategory;
-
-public record ProductInputDto(ProductCategory category, String kind, String name, String detail, int numOf, int price) {
-}
-
